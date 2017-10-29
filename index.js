ce08a3d455fdd22523b340e96e880a2e60ded94a
rebeccanewborn <rjn58@cornell.edu>
2017-10-29 12:26:06 -0400

    lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+
+let currentUser = "Becky Newborn";
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+let shortGreeting = `Welcome, ${currentUser[0]}!`
